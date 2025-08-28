5541933cf6803b026359ae4c62092f30ce0a56f7
Krystian Mikołajczyk <krystianb127098@outlook.com>
2025-08-28 22:02:18 +0200

    Remove unused font dependency and related code from layout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import { HeaderBackButton } from '@react-navigation/elements';
 import { DarkTheme, DefaultTheme, ThemeProvider, useNavigation } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
@@ -12,13 +11,6 @@ import { useColorScheme } from '../hooks/useColorScheme';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
